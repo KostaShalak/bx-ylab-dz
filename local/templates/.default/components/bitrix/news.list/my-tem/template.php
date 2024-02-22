207b37fdc32e005ccc5a1369797a572bb0222afc
Kosta Shalak <zapalka9991@mail.ru>
2024-02-22 20:59:55 +0300

    first commit to dz1

---
+++ local/templates/.default/components/bitrix/news.list/my-tem/template.php
@@ -0,0 +1,45 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+
+?>
+<div class="news-list my-style">
+    <div class="container">
+        <?
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+
+        <?foreach($arResult["ITEMS"] as $arSectItem): ?>
+
+            <h2><?echo $arSectItem['NAME']?></h2>
+
+            <? if($arSectItem['ELEMENTS']):?>
+
+                <?foreach($arSectItem['ELEMENTS'] as $arItem): ?>
+                    <?$arProps = $arItem['DISPLAY_PROPERTIES'];?>
+
+                    <h4><?echo $arItem["NAME"]?></h4>
+                    <p><?echo $arItem["PREVIEW_TEXT"]?></p>
+                    <p><?echo $arItem["DETAIL_TEXT"] . " ". $arProps["WEIGHT"]["VALUE"] . $arProps["UNIT"]["VALUE"]?></p>
+<!--                    <pre>--><?//print_r($arItem)?><!--</pre>-->
+                <?endforeach;?>
+            <?endif;?>
+
+        <?endforeach;?>
+
+        <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+            <br /><?=$arResult["NAV_STRING"]?>
+        <?endif;?>
+    </div>
+</div>
